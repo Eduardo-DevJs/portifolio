5dbf4025577fd9ee5dc8a400802df235ca37dc7b
Eduardo-DevJs <dudev2001@gmail.com>
2024-05-05 03:50:52 -0300

    Links

--- src/components/CardProfile/index.tsx
+++ src/components/CardProfile/index.tsx
@@ -1,5 +1,5 @@
 import { FaRegCopy, FaWhatsapp } from "react-icons/fa";
-import { FiFacebook, FiGithub, FiLinkedin } from "react-icons/fi";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { useTheme } from "../../Contexts/ThemeContext";
 import PhotoProfile from "../../assets/profile-removebg-preview.png";
 
@@ -18,7 +18,9 @@ export default function CardProfile() {
   const isChangedTheme = theme === "light" ? "bg-white" : "bg-slate-900";
 
   return (
-    <aside className={`${isChangedTheme} col-span-2 md:col-span-1 shadow p-5 rounded`}>
+    <aside
+      className={`${isChangedTheme} col-span-2 md:col-span-1 shadow p-5 rounded`}
+    >
       <div className="space-y-5">
         <div
           className={`${
@@ -36,8 +38,8 @@ export default function CardProfile() {
         </h1>
         <p className="text-slate-500 leading-8 font-semibold">
           Desenvolvedor Frontend, recém formado em desenvolvimento de sistemas
-          pela SENAI no estado do Espirito Santo. Apaixonado por técnologias
-          e por ensinar! 👨‍💻
+          pela SENAI no estado do Espirito Santo. Apaixonado por técnologias e
+          por ensinar! 👨‍💻
         </p>
         <div className="flex items-center gap-2">
           <button className="p-4 flex items-center gap-4 bg-green-600 rounded font-bold text-white">
@@ -60,19 +62,15 @@ export default function CardProfile() {
 
         <div className="flex items-center gap-5">
           <a
-            href="#"
-            className="border border-gray-40 p-2 rounded text-slate-500 hover:text-sky-500"
-          >
-            <FiFacebook size={24} />
-          </a>
-          <a
-            href="#"
+            href="https://www.linkedin.com/in/eduardo-oliveira-liria-849467246/"
+            target="_blank"
             className="border border-gray-40 p-2 rounded text-slate-500 hover:text-sky-500"
           >
             <FiLinkedin size={24} />
           </a>
           <a
-            href="#"
+            href="https://github.com/Eduardo-DevJs"
+            target="_blank"
             className="border border-gray-40 p-2 rounded text-slate-500 hover:text-sky-500"
           >
             <FiGithub size={24} />
